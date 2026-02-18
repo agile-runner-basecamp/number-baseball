07aa25ce56d503dbf21f7f3c42e5307ea622db8a
이승환 <seaung13@gmail.com>
2026-02-19 07:36:44 +0900

    [ECS-14] feat: 심판 기능 추가

---
+++ java/src/main/java/baseball/Umpire.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Umpire {
+
+    public int[] makeCall(int numbers, int target) {
+        // Todo: 리팩토링
+        return new int[]{0, 1};
+    }
+}
---
+++ java/src/test/java/baseball/UmpireTest.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UmpireTest {
+
+    @DisplayName("각 자리수가 정답과 서로 같은지 판단한다.")
+    @Test
+    void makeCall() {
+        // given (재료 준비)
+        int numbers = 123;
+        int target = 145;
+
+        Umpire umpire = new Umpire();
+
+        // when (검증 대상)
+        int[] result = umpire.makeCall(numbers, target);
+
+
+        // then (예상 결과)
+        assertThat(result).containsExactly(new int[]{0, 1});
+    }
+
+}
